3055accd990856c74e7ed9fd2afd591cf56d12c4
klauspatriota <klauspatriota@icloud.com>
2022-08-30 23:04:17 -0300

    “pratica-1”

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,13 +3,8 @@ let email = "klauspatriota@icloud.com"
 let profissao = "Funcionario Publico"
 let idade = 45
 let viagemDosSonhos = "Paris"
-
-let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
-
-console.log(mensagem)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Securit
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDosSonhos)
